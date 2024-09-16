03384229b02f68eb6fd604468db913622ff72024
Marina Elis de Morais <marinamorais@aluno.senai.br>
2024-09-16 11:52:10 -0300

    feat: Add Header component to Feedback page

---
+++ app/components/header/Header.jsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import styles from "./header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.leftSide}>
+                <img className={styles.img} src="https://i.imgur.com/MFSxuJ3.png" alt="Senai" />
+            </div>
+            <div className={styles.divider}></div> {/* Divisória */}
+            <div className={styles.rightSide}>
+                <h1 className={styles.h1}>Pesquisa de Satisfação</h1>
+            </div>
+        </header>
+    );
+}
+
+
--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import Feedback from "./components/feedback/Feedback";
 import GraficoPizza from "./components/graficoPizza/GraficoPizza";
 import GraficoTempo from "./components/graficoTempo/GraficoTempo";
 
+
 export default function Home() {
     const [usuarios, setUsuarios] = useState([]);
 
