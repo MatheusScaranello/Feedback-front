c900877061cab207f86792f6c1a4a03e9e39aa51
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-09-18 17:20:05 -0300

    aa

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
                     </li>
                 ))}
             </ul> */}
-            <Feedback />
+            <GraficoTempo />
         </>
     );
 }
