d0b78dd244e458570b5c24c7ac6cc1abc1000979
Marinamorais <marinamorais@aluno.senai.br>
2024-09-30 14:28:09 -0300

    Refactorizar componente Feedback y Header
    - Refactorizar el componente Feedback para mejorar la estructura del código y eliminar código innecesario.
    - Refactorizar el componente Header para mejorar la estructura del código y eliminar código innecesario.

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -2,31 +2,24 @@
 import React from "react";
 import styles from "./header.module.css";
 
-
 const Header = (props) => {
-{
-
-export default function Header() {
-
     return (
         <header className={styles.header}>
             <div className={styles.leftSide}>
-                <img className={styles.img} src="https://i.imgur.com/MFSxuJ3.png" alt="Senai" />
+                <img
+                    className={styles.img}
+                    src="https://i.imgur.com/MFSxuJ3.png"
+                    alt="Senai"
+                />
             </div>
             <div className={styles.divider}></div> {/* Divisória */}
             <div className={styles.rightSide}>
-
                 <h1 className={styles.h1}>{props.title}</h1>
-
                 <h1 className={styles.h1}>Pesquisa de Satisfação</h1>
-
             </div>
         </header>
     );
-}
-
-}
-
+};
 
 export default Header;
 
